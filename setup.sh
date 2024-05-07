7a41a18b11278cf54feb1514e08b4e902a8602fe
“AddiH” <astrid.elmann@gmail.com>
2024-05-07 16:26:47 +0200

    end of day

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install python3-venv
 
 # make a new env
-python3 -m venv env/airbnb
+#python3 -m venv env/airbnb
 
 # activate the enviroment for the assignment
 source ./env/airbnb/bin/activate
@@ -13,7 +13,7 @@ python3 -m pip install --upgrade pip # ucloud version is outdated
 python3 -m pip install -r requirements.txt
 
 # download data
-bash data/kaggle.sh
+#bash data/kaggle.sh
 
 # activate kernel
 python -m ipykernel install --user --name=airbnb
